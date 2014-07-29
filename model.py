b18e46a1045c156ca4c81379e86aad37247bac2a
lis <lisazhou@My-MacBook.local>
2014-07-29 11:06:55 -0700

    working API call; rendering 5 wine recommendations onto template (pt. 2)

--- model.py
+++ model.py
@@ -39,6 +39,29 @@ class WineObject(Base):
 def createTable():
     Base.metadata.create_all(ENGINE)
 
+class WineRecommendations(Base):
+    __tablename__ = "wine_recommendations"
+    id = Column(Integer, primary_key = True)
+    varietal_id = Column(String, ForeignKey('wineobject.varietal'), nullable=False)
+    label_image = Column(String(250), nullable=True)
+    wine_name = Column(String(250), nullable=True)
+    wine_rating = Column(String(3), nullable=True)
+    retail_price = Column(String(7), nullable=True)
+
+    variety = relationship("WineObject", backref=backref("wine_recommendations)", order_by=id))
+
+def add_api_request_to_db(session, api_request, varietal_info):
+    for wine_rec in api_request["Products"]["List"]:
+        recommendation = WineRecommendations(
+            varietal_id=varietal_info.varietal,
+            label_image = wine_rec["Labels"][0]["Url"],
+            wine_name=wine_rec["Name"],
+            wine_rating=wine_rec["Ratings"]["HighestScore"],
+            retail_price=wine_rec["PriceRetail"])
+
+        session.add(recommendation)
+    session.commit()
+
 def get_wine_types(wine_type):
     cursor = connect()
     wine_type = wine_type.capitalize()
@@ -47,8 +70,12 @@ def get_wine_types(wine_type):
 
 def get_varietal(wine_id):
     cursor = connect()
-    varietal_desc = dbsession.query(WineObject).filter_by(id=wine_id).all()
+    varietal_desc = dbsession.query(WineObject).filter_by(id=wine_id).one()
     return varietal_desc
+
+# def get_cached_wine_list(wine_id):
+#     cursor = connect()
+
 
 def get_api_parameters(wine_id):
     cursor = connect()
@@ -56,11 +83,12 @@ def get_api_parameters(wine_id):
     return wine_object[0].api_id
 
 def make_api_call(wine_id):
-    # varietal_objects = get_varietal(wine_id)
     parameters = get_api_parameters(wine_id)
     url = 'http://services.wine.com/api/beta2/service.svc/json/catalog?filter=categories(490+'+parameters+')&filter=rating(93|100)&apikey='+API_KEY
     api_request = requests.get(url).json()
     return api_request
+    add_api_request_to_db()
+
 
 def connect():
     global dbsession
--- vino.py
+++ vino.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 app.secret_key = '\xf5!\x07!qj\xa4\x08\xc6\xf8\n\x8a\x95m\xe2\x04g\xbb\x98|U\xa2f\x03'
 app.jinja_env.undefined = jinja2.StrictUndefined
 
-API_KEY = 'c02a2b186d8415fbd1ea09519abffa5a'
-
 @app.route("/")
 def index():
     """This is the cover page of the Vinfluence web app."""
@@ -16,18 +14,26 @@ def index():
 
 @app.route("/type/<wine_type>")
 def show_wine_type(wine_type):
+    """This is the <type> page. Displays the carousel of various varietals of the wine type selected by user from previous page. """
     varietal_list = model.get_wine_types(wine_type)
     return render_template("/type.html", varietal_object=varietal_list, wine_type=wine_type)
 
 @app.route("/varietal/<id>")
 def show_varietal_description(id):
+    """This is the <varietal> page. It will display descriptions of the varietal, including food for pairing as well as wine recommendations from the wine.com API."""
     varietal_description = model.get_varietal(id)
+#check to see if exist in DB
+    # wine_rec_list = 
+#else
     api_request = model.make_api_call(id)
-    return render_template("/varietal.html", varietal_description=varietal_description, api_request=api_request)
+    add_api_request_to_db = model.add_api_request_to_db(model.dbsession, api_request, varietal_description)
 
-# @app.route("/login")
-# def login():
-#     return render_template("login.html")
+    return render_template("/varietal.html", varietal=varietal_description, api_request=api_request)
 
+"""Phase 2 feature:
+@app.route("/login")
+def login():
+    return render_template("login.html")
+"""
 if __name__ == "__main__":
     app.run(debug = True)
