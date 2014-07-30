f897c19b9697e12d3b09514288e6b87c9978172b
lis <lisazhou@My-MacBook.local>
2014-07-30 12:58:59 -0700

    refactored code and comments added for clarity; added but not linked to phase 2's login

--- model.py
+++ model.py
@@ -7,12 +7,12 @@ from sqlalchemy.orm import relationship, backref
 from urllib2 import urlopen
 import requests
 
-API_KEY = 'c02a2b186d8415fbd1ea09519abffa5a'
+API_KEY = 'xxxxxxxxxx'
 
 ENGINE = None
 Session = None
 
-ENGINE = create_engine("sqlite:///winelist.db", echo = False)
+ENGINE = create_engine("sqlite:///winelist.db", echo = True)
 dbsession = scoped_session(sessionmaker(bind = ENGINE,
                                     autocommit = False,
                                     autoflush = False))
@@ -41,61 +41,77 @@ def createTable():
 
 class WineRecommendations(Base):
     __tablename__ = "wine_recommendations"
+
     id = Column(Integer, primary_key = True)
     varietal_id = Column(String, ForeignKey('wineobject.varietal'), nullable=False)
     label_image = Column(String(250), nullable=True)
     wine_name = Column(String(250), nullable=True)
     wine_rating = Column(String(3), nullable=True)
     retail_price = Column(String(7), nullable=True)
+    wine_url = Column(String(500), nullable=True)
 
     variety = relationship("WineObject", backref=backref("wine_recommendations)", order_by=id))
 
-def add_api_request_to_db(session, api_request, varietal_info):
-    for wine_rec in api_request["Products"]["List"]:
-        recommendation = WineRecommendations(
-            varietal_id=varietal_info.varietal,
-            label_image = wine_rec["Labels"][0]["Url"],
-            wine_name=wine_rec["Name"],
-            wine_rating=wine_rec["Ratings"]["HighestScore"],
-            retail_price=wine_rec["PriceRetail"])
-
-        session.add(recommendation)
-    session.commit()
-
 def get_wine_types(wine_type):
+# Gets list of wine varietals from wineobject table in db for use in carousel.
     cursor = connect()
     wine_type = wine_type.capitalize()
     varietals = dbsession.query(WineObject).filter_by(wine_type=wine_type).all()
     return varietals
 
 def get_varietal(wine_id):
+# Retrieves all associated info for the inputted varietal (in this case, the id for the varietal is "wine_id") from the wineobject table.
     cursor = connect()
     varietal_desc = dbsession.query(WineObject).filter_by(id=wine_id).one()
     return varietal_desc
 
-# def get_cached_wine_list(wine_id):
-#     cursor = connect()
-
-
 def get_api_parameters(wine_id):
+# Gets the parameters/api_id for the varietal ("wine_id") as defined on wine.com
     cursor = connect()
     wine_object = dbsession.query(WineObject).filter_by(id=wine_id).all()
     return wine_object[0].api_id
 
 def make_api_call(wine_id):
+# Makes an API call to wine.com
+# API request will return JSON of a list of wines (with parameters: varietal, rated 93+)
     parameters = get_api_parameters(wine_id)
     url = 'http://services.wine.com/api/beta2/service.svc/json/catalog?filter=categories(490+'+parameters+')&filter=rating(93|100)&apikey='+API_KEY
     api_request = requests.get(url).json()
     return api_request
     add_api_request_to_db()
 
+def add_api_request_to_db(session, api_request, varietal_info):
+# Adds the following pieces of information of each wine ("wine_rec") from the list of wines from API request into wine_recommendations table in db: 
+#     - varietal name (varietal_id), 
+#     - url to wine label image (label_image), 
+#     - wine name (wine_name), 
+#     - highest rating score of the wine on wine.com (wine_rating), 
+#     - retail price of the bottle of wine (retail_price),
+#     - url to the wine's product page on wine.com (wine_url)
+    for wine_rec in api_request["Products"]["List"]:
+        recommendation = WineRecommendations(
+            varietal_id=varietal_info.varietal,
+            label_image = wine_rec["Labels"][0]["Url"],
+            wine_name=wine_rec["Name"],
+            wine_rating=wine_rec["Ratings"]["HighestScore"],
+            retail_price=wine_rec["PriceRetail"],
+            wine_url=wine_rec["Url"])
+
+        session.add(recommendation)
+    session.commit()
+
+def get_cached_wine_list(wine_id):
+#Gets the list of wine recommendations (first originated from the wine.com API request)
+    cursor = connect()
+    recommendation_list = dbsession.query(WineRecommendations).filter_by(varietal_id=wine_id).all()
+    return recommendation_list
 
 def connect():
     global dbsession
     return dbsession()
 
 def main():
-    # createTable()
+    createTable()
     pass
     
 if __name__ == "__main__":
--- vino.py
+++ vino.py
@@ -9,31 +9,44 @@ app.jinja_env.undefined = jinja2.StrictUndefined
 
 @app.route("/")
 def index():
-    """This is the cover page of the Vinfluence web app."""
+# This is the cover page of the VinFluence web app.
     return render_template("index.html")
 
 @app.route("/type/<wine_type>")
 def show_wine_type(wine_type):
-    """This is the <type> page. Displays the carousel of various varietals of the wine type selected by user from previous page. """
+# This is the <type> page -- red or white. Displays a carousel with various varietals associated with the wine type.
     varietal_list = model.get_wine_types(wine_type)
     return render_template("/type.html", varietal_object=varietal_list, wine_type=wine_type)
 
 @app.route("/varietal/<id>")
 def show_varietal_description(id):
-    """This is the <varietal> page. It will display descriptions of the varietal, including food for pairing as well as wine recommendations from the wine.com API."""
+# This is the <varietal> page. 
+# Display descriptions of the varietal, including pairing suggestions and wine recommendations.
+# Will first check in db for wine recommendations list to render. If list is empty in database for the selected varietal, will call a function to make an API call to wine.com to retrieve missing data and cache into db. 
     varietal_description = model.get_varietal(id)
-#check to see if exist in DB
-    # wine_rec_list = 
-#else
-    api_request = model.make_api_call(id)
-    add_api_request_to_db = model.add_api_request_to_db(model.dbsession, api_request, varietal_description)
-
+    wine_rec_list = model.get_cached_wine_list(varietal_description.varietal)
+    if wine_rec_list != []:
+        api_request = {}
+        api_request["Products"] = {}
+        api_request["Products"]["List"] = []
+        for wine in wine_rec_list:
+            wine_item = {}
+            wine_item["Name"] = wine.wine_name
+            wine_item["Labels"] = [{"Url": wine.label_image}]
+            wine_item["Ratings"] = {"HighestScore": wine.wine_rating}
+            wine_item["PriceRetail"] = float(wine.retail_price)
+            wine_item["Url"] = wine.wine_url
+            api_request["Products"]["List"].append(wine_item)
+    else:
+        api_request = model.make_api_call(id)
+        add_api_request_to_db = model.add_api_request_to_db(model.dbsession, api_request, varietal_description)
+   
     return render_template("/varietal.html", varietal=varietal_description, api_request=api_request)
 
-"""Phase 2 feature:
+"""Phase 2 feature:"""
 @app.route("/login")
 def login():
     return render_template("login.html")
-"""
+
 if __name__ == "__main__":
     app.run(debug = True)
